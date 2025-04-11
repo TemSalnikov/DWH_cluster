751e6d51be7fccc8c0c7e4f4d3ace4d7516cb28b
TemSalnikov <twindt@email.ru>
2025-04-11 16:54:43 +0300

    правки при заппуске на машине Алтайвитамин

--- ch_kafka_af_superset_without_swarm/superset/docker_entrypoint.sh
+++ ch_kafka_af_superset_without_swarm/superset/docker_entrypoint.sh
@@ -2,5 +2,9 @@
 
 echo "Start install clickhouse-connect"
 pip install clickhouse-connect
+echo "Start install psycopg2-binary"
+pip install psycopg2-binary
+echo "Init superset"
+./superset_init.sh
 echo "Start superset"
 superset run -h 0.0.0.0 -p 8088
