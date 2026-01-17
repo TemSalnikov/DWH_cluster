201cbab3d0d4752a7c2fe6b5c53950b78900b876
temski22 <temski22@gmail.com>
2026-01-17 18:36:44 +0000

    правка докеров

--- ch_kafka_af_superset/superset/docker_entrypoint.sh
+++ ch_kafka_af_superset/superset/docker_entrypoint.sh
@@ -4,7 +4,9 @@ echo "Start install clickhouse-connect"
 pip install clickhouse-connect
 echo "Start install psycopg2-binary"
 pip install psycopg2-binary
+echo "Start install Pillow"
+pip install Pillow
 echo "Init superset"
-./superset_init.sh
+/docker-entrypoint-initdb.d/superset_init.sh
 echo "Start superset"
 superset run -h 0.0.0.0 -p 8088
--- ch_kafka_af_superset/superset/superset_config.py
+++ ch_kafka_af_superset/superset/superset_config.py
@@ -6,22 +6,37 @@ SECRET_KEY = 'thisISaSECRET_1234'
 MAPBOX_API_KEY = os.getenv('MAPBOX_API_KEY', '')
 CACHE_CONFIG = {
     'CACHE_TYPE': 'redis',
-    'CACHE_DEFAULT_TIMEOUT': 300,
+    'CACHE_DEFAULT_TIMEOUT': 0,
+    # 'CACHE_DEFAULT_TIMEOUT': 300,
     'CACHE_KEY_PREFIX': 'superset_',
     'CACHE_REDIS_HOST': 'redis',
     'CACHE_REDIS_PORT': 6379,
     'CACHE_REDIS_DB': 1,
     'CACHE_REDIS_URL': 'redis://redis:6379/1'}
+
 SQLALCHEMY_DATABASE_URI = \
     'postgresql+psycopg2://superset:superset@postgres:5432/superset'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 # Увеличьте время ожидания соединения
 SUPERSET_WEBSERVER_TIMEOUT = 300
 
+# GLOBAL_ASYNC_QUERIES_JWT_SECRET = "YOUR_STRONG_32_BYTE_SECRET_KEY"
 
 FEATURE_FLAGS = {
     "GENERIC_CHART_AXES": True,
+    # "GLOBAL_ASYNC_QUERIES": True,
+    # "ASYNC_EXPORT": True,
+    
 }
+
+# Добавление русского языка в список доступных языков
+LANGUAGES = {
+    "ru": {"flag": "ru", "name": "Русский"},
+    "en": {"flag": "us", "name": "English"}    
+}
+
+# Установка русского языка в качестве языка по умолчанию
+BABEL_DEFAULT_LOCALE = "ru"
 
 # Конфигурация базы данных
 DATAABASE_POOL_SIZE = 10
